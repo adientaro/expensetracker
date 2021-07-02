243da276273519f0f4c7ccc422665092cd14d392
Paweł <pawel.kowalski@comarch.com>
2021-07-02 11:41:20 +0200

    Add expense view

--- app/src/Expenses.js
+++ app/src/Expenses.js
@@ -1,79 +1,207 @@
-import React, { Component } from "react";
-import DatePicker from "react-datepicker";
-import { Container, Input, Button, Label, Form, FormGroup } from "reactstrap";
-import AppNav from "./AppNav";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import AppNav from './AppNav';
+import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import './App.css';
+import { Table,Container,Input,Button,Label, FormGroup, Form} from 'reactstrap';
+import {Link} from 'react-router-dom';
+import Moment from 'react-moment';
 
-class Expenses extends Component {
-  state = {
-    date: new Date(),
-    isLoading: true,
-    expenses: [],
-    categories: []
-  };
+class Expsenses extends Component {
+ 
+    emptyItem = {
+        description : '' ,
+        expensedate : new Date(),
+        id:104,
+        category : {id:1 , name:'Travel'}
+    }
 
-  async componentDidMount() {
-    const respose = await fetch("/api/categories");
-    const body = await respose.json();
-    this.setState({categories: body, isLoading: false });
-  }
+    
+    constructor(props){
+      super(props)
 
-  render() {
-    const title = <h3>Adding expense</h3>;
-    const { categories, isLoading } = this.state;
+      this.state = { 
+        isLoading :false,
+        Categories:[],
+        Expsenses : [],
+        date :new Date(),
+        item : this.emptyItem
+       }
 
-    if (isLoading) return (<div>Loading ...</div>)
+       this.handleSubmit= this.handleSubmit.bind(this);
+       this.handleChange= this.handleChange.bind(this);
+       this.handleDateChange= this.handleDateChange.bind(this);
 
-    let optionList = categories.map((category) => (
-        <option id={category.id}>{category.name}</option>
-      ))
+    } 
 
-    return (
-      <div>
-        <AppNav />
-        <Container>
-          {title}
-          <Form>
-            <FormGroup>
-              <label for="title">Title</label>
-              <input
-                type="text"
-                name="title"
-                id="title"
-                onChange={this.handleChange}
-              ></input>
-            </FormGroup>
+    async handleSubmit(event){
+     
+      const item = this.state.item;
+    
 
-            <FormGroup>
-              <label for="category">Category</label>
-              <select>
-                  {optionList}
-              </select>
-            
-            </FormGroup>
+      await fetch(`/api/expenses`, {
+        method : 'POST',
+        headers : {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body : JSON.stringify(item),
+      });
+      
+      event.preventDefault();
+      this.props.history.push("/expenses");
+    }
 
-            <FormGroup>
-              <label for="Date">Expense date</label>
-              <DatePicker
-                selected={this.state.date}
-                onChange={this.handleChange}
-              ></DatePicker>
-            </FormGroup>
 
-            <FormGroup>
-              <Button color="primary" type="submit">
-                Save
-              </Button>{" "}
-              <Button color="secondary" tag={Link} to="/categories">
-                Cancel
-              </Button>
-            </FormGroup>
-          </Form>
-        </Container>
-      </div>
-    );
-  }
+    handleChange(event){
+      const target= event.target;
+      const value= target.value;
+      const name = target.name;
+      let item={...this.state.item};
+      item[name] = value;
+      this.setState({item});
+      console.log(item);
+    }
+
+
+    handleDateChange(date){
+      let item={...this.state.item};
+      item.expensedate= date;
+      this.setState({item});
+    
+    }
+
+
+
+
+
+
+    async remove(id){
+        await fetch(`/api/expenses/${id}` , {
+          method: 'DELETE' ,
+          headers : {
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+          }
+
+        }).then(() => {
+          let updatedExpenses = [...this.state.Expsenses].filter(i => i.id !== id);
+          this.setState({Expsenses : updatedExpenses});
+        });
+
+    }
+
+
+    async componentDidMount() {
+ 
+     
+
+        const response= await fetch('/api/categories');
+        const body= await response.json();
+        this.setState({Categories : body , isLoading :false});
+
+
+        const responseExp= await fetch('/api/expenses');
+        const bodyExp = await responseExp.json();
+        this.setState({Expsenses : bodyExp , isLoading :false});
+        console.log(bodyExp);
+
+    }
+
+
+
+
+
+    render() { 
+        const title =<h3>Add Expense</h3>;
+        const {Categories} =this.state;
+        const {Expsenses,isLoading} = this.state;
+        
+
+        if (isLoading)
+            return(<div>Loading....</div>)
+        
+
+
+        let optionList  =
+                Categories.map( (category) =>
+                    <option value={category.id} key={category.id}>
+                                {category.name} 
+                    </option>
+                )
+        
+        let rows=
+            Expsenses.map( expense =>
+              <tr key={expense.id}>
+                <td>{expense.description}</td>
+                <td><Moment date={expense.expensedate} format="YYYY/MM/DD"/></td>
+                <td>{expense.category.name}</td>
+                <td><Button size="sm" color="danger" onClick={() => this.remove(expense.id)}>Delete</Button></td>
+
+              </tr>
+
+
+            )
+        
+
+        return (
+            <div>
+                <AppNav/>
+                <Container>
+                    {title}
+                    
+                    <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="description">Title</Label>
+                        <Input type="description" name="description" id="description" 
+                            onChange={this.handleChange} autoComplete="name"/>
+                    
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="category">Category</Label>
+                        <select onChange={this.handleChange}>
+                                {optionList}
+                        </select>
+                    
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="Date">Date</Label>
+                        <DatePicker    selected={this.state.item.expensedate}  onChange={this.handleDateChange} />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>{' '}
+                        <Button color="secondary" tag={Link} to="/">Cancel</Button>
+                    </FormGroup>
+                    </Form>
+                </Container>
+              
+
+          {''}
+              <Container>
+                <h3>Expense List</h3>
+                <Table className="mt-4">
+                <thead>
+                  <tr>
+                    <th width="30%">Description</th>
+                    <th> Date</th>
+                    <th width="20%">Category</th>
+                    <th width="10%">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                   {rows}
+                </tbody>
+
+                </Table>
+              </Container>
+
+        </div>
+
+        );
+    }
 }
-
-export default Expenses;
+ 
+export default Expsenses;
---
+++ src/main/java/com/expensetracker/expensetracker/repository/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package com.expensetracker.expensetracker.repository;
+
+import com.expensetracker.expensetracker.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+
+}
